45fa1e5553cf0e178084e2a52b3c34b0fb98dd18
sakib <sakibs8@gmail.com>
2015-02-11 06:07:28 +0000

    modified files to use angular resource

--- client/js/controllers/meetups-controller.js
+++ client/js/controllers/meetups-controller.js
@@ -1,3 +1,17 @@
-function meetupsController($scope) {
-	$scope.meetupsCount = 10;
-}
+app.controller('meetupsController', ['$scope', '$resource', 
+	function ($scope, $resource) {
+		$scope.meetupsCount = 10;
+
+		$scope.meetups = [
+			{ name:"spontaneous" },
+			{ name:"meeting 2" }
+		]
+
+		$scope.createMeetup = function() {
+			// dont take args because we can access variable meetupName from form
+			$scope.meetups.push({ name: $scope.meetupName });
+			// clean up web form input text so placeholder is used
+			$scope.meetupName = '';
+		}
+	}
+]);
